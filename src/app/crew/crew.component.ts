75cfe4506e4034243c2c19ec35005fb143db9845
Kelsey Niederer <kelseyniederer12@gmail.com>
2020-04-07 20:45:16 -0500

    finished studio 2

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -13,7 +13,27 @@ export class CrewComponent implements OnInit {
     {name: "Ellen Ochoa", firstMission: true}
   ];
 
+  memberBeingEdited: object = null;
+
   constructor() { }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
 
   ngOnInit() {
   }
